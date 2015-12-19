986446e9bfa6e0b246564fd9132d768402f1740a
yorung <yorung@gmail.com>
2015-12-19 20:32:11 +0900

    Limit player's area

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,13 +13,23 @@ public class Player : MonoBehaviour {
             yield return new WaitForSeconds(spaceship.shotDelay);
         }
     }
-
+    void Move(Vector2 direction)
+    {
+        Vector2 min = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+        Vector2 max = Camera.main.ViewportToWorldPoint(new Vector2(1, 1));
+        Vector2 pos = transform.position;
+        pos += direction * spaceship.speed * Time.deltaTime;
+        pos.x = Mathf.Clamp(pos.x, min.x, max.x);
+        pos.y = Mathf.Clamp(pos.y, min.y, max.y);
+        transform.position = pos;
+    }
     void Update()
     {
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
         Vector2 direction = new Vector2(x, y).normalized;
-        spaceship.Move(direction);
+//        spaceship.Move(direction);
+        Move(direction);
     }
 
     void OnTriggerEnter2D(Collider2D c)
