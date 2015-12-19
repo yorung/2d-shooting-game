b4c3463b500a01c311477b8c5034395222e819c7
yorung <yorung@gmail.com>
2015-12-19 19:50:09 +0900

    Bullet removal

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
     public int speed = 10;
-
+    public float lifeTime = 5;
     void Start()
     {
         GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+        Destroy(gameObject, lifeTime);
     }
 }
