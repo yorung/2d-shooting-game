c8196c3710202d300b8e767c2d8a8b23f70d7c88
yorung <yorung@gmail.com>
2015-12-19 19:59:20 +0900

    background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Background : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    void Update()
+    {
+        float y = Mathf.Repeat(Time.time * speed, 1);
+        Vector2 offset = new Vector2(0, y);
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
